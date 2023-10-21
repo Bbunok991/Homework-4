fa001a9a0dc47b814404a44036ac78338f2127d3
Bbuno <Bbuno@HACKER_PC>
2023-10-22 00:39:05 +0300

    Добавьте файлы проекта.

---
+++ FindNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    internal class FindNumbers
+    {
+        public List<int> FindThreeNumbers(int[] arr, int target)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                HashSet<int> set = new HashSet<int>();
+
+                int currentSum = target - arr[i];
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (set.Contains(arr[j]))
+                    {
+                        result.Add(arr[i]);
+                        result.Add(arr[j]);
+                        result.Add(currentSum - arr[j]);
+                        return result;
+                    }
+
+                    set.Add(currentSum - arr[j]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FindNumbers findThreeNumbers = new FindNumbers();
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            int target = 10;
+
+            List<int> result = findThreeNumbers.FindThreeNumbers(arr, target);
+
+            if (result.Count > 0)
+            {
+                Console.WriteLine("Три числа сумма которых равна " + target + ":");
+                foreach (int num in result)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Не удалось найти три числа сумма которых равна " + target);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
